2ae91cdbeb565b9d3926ee55f09e605c893b2fb3
sulejmanbesic03-stack <sulejman.besic03@gmail.com>
2026-09-06 08:35:57 +0200

    Fix cancellation wrapper and JSON build errors

--- AI Assistant/AgentV2/UnityCommandExecutorCurrent.cs
+++ AI Assistant/AgentV2/UnityCommandExecutorCurrent.cs
@@ -4,6 +4,7 @@ using AI_Assistant.Tools;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Threading;
